33174b492e40ba79b55962d2140992b6154e1c69
Anton <anton@magharmoni.se>
2019-01-25 15:47:23 +0100

    updated text instructions

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,6 +7,9 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Give it at try.'}
             </p>
+            <p className='f5'>
+                {'Enter the webbaddress of a picture. You can try this address as an example: http://a.espncdn.com/photo/2018/0726/r406100_800x450_16-9.jpg '}
+            </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                     <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
