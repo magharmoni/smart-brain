efde5bb7d9481d09cb74278e607bdf1b267281c9
Anton <anton@magharmoni.se>
2019-03-15 14:10:05 +0100

    added rank badge with  aws Lambda and docke

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,10 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3'>
-                {'This Magic Brain will detect faces in your pictures. Enter the webbaddress of a picture and give it at try.'}
-            </p>
-            <p className='f5'>
-                {'As an example, you may copy and enter this picture address: http://a.espncdn.com/photo/2018/0726/r406100_800x450_16-9.jpg '}
+                {'This Magic Brain will detect faces in your images. Enter the link of an image and give it at try.'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
@@ -16,6 +13,9 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
                     <button className='f4 pa2 grow w-30 link ph3 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
+            <p className='f5'>
+                {'As an example, copy and enter this address: http://a.espncdn.com/photo/2018/0726/r406100_800x450_16-9.jpg '}
+            </p>
         </div>
     );
 }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,17 +1,46 @@
 import React from 'react';
 
-const Rank = ({name, entries}) => {
-    console.log(name, entries);
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    }
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.entries === this.props.entries && this.props.prevProps === this.props.name) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://twv7o0ex20.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`)
+    .then(response => response.json())
+    .then(data => this.setState({emoji: data.input}))
+    .catch(console.log)
+  }
+
+  render() {
     return (
-        <div>
-            <div className='white f3'>
-              {`${name}, your current entry count is...`}
-            </div>
-            <div className='white f1'>
-              { entries }
-            </div>
-        </div>
+      <div>
+          <div className='white f3'>
+            {`${this.props.name}, your current entry count is...`}
+          </div>
+          <div className='white f1'>
+            { this.props.entries }
+          </div>
+          <div>
+            {`Badge rank: ${this.state.emoji}`}
+          </div>
+      </div>
     );
+  }
 }
 
 export default Rank;
