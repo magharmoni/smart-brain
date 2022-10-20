d26a1d40206dc3b7757e04977a07331f49bb8c47
Anton <anton@magharmoni.se>
2022-10-20 19:41:23 +0200

    updated all packages

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Particles from 'react-particles-js';
+import ParticlesBg from 'particles-bg';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
 
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
 
 const intialState = {
   input: '',
@@ -122,9 +122,7 @@ loadUser = (data) => {
     const {imageUrl, route, boxes, user, isSignedIn } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
-              params={particlesOptions}
-            />
+        <ParticlesBg type="circle" bg={true} />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
         { route === 'home'
           ? <div>
